1c24adb8f6e30ffffa0fa5df011ef251bd4599ec
Amer-bit <ny.1995@outlook.com>
2020-07-25 15:23:53 +0300

    added a couble routes in the header

---
+++ src/components/header/header.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Header = (props) => {
+  return (
+    <>
+      <header>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+                <Link to="/Administration">Administration</Link>
+            </li>
+            <li>
+                <Link to="/financials">Financia Manage</Link>
+            </li>
+            <li>
+                <Link to="/profile">Profile</Link>
+            </li>
+            <li>
+                <Link to="/signin">Sign-in</Link>
+            </li>
+            <li>
+                <Link to="/signup">Sign-up</Link>
+            </li>
+            <li>
+                <Link to="/signout">Sign-out</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
+  );
+};
+
+export default Header;
