05e875fb0cd9765485370da8e7eb25a14391183b
Abdallah <eng.abdallahobaid@gmail.com>
2020-07-26 23:43:33 +0300

    w

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -9,7 +9,7 @@ const Header = (props) => {
     <>
       <nav className="navbar navbar-expand-lg navbar-light bg-light shadow fixed-top">
         <div className="container">
-          <a className="navbar-brand" href="#">EMS</a>
+          <a className="navbar-brand" href="/">EMS</a>
           <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
