d3ab10a29f3b6bea06eb111ea5d039c877f775b3
Abdallah <eng.abdallahobaid@gmail.com>
2020-07-27 03:14:12 +0300

    fix some issue

--- src/components/Home/home.js
+++ src/components/Home/home.js
@@ -2,16 +2,13 @@
 import React from 'react';
 // import { Link } from 'react-router-dom';
 import Slider from '../slider/slider';
-import Header from '../header/header';
-import Footer from '../footer/footer';
+
     
 
 const Home = (props) => {
   return (
     <>
-      {/* <Header /> */}
       <Slider />
-      {/* <Footer /> */}
     </>
   );
 };
