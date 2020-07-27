6392544e823a19f2c487fda0c8490455a48914d0
Abdallah <eng.abdallahobaid@gmail.com>
2020-07-27 10:07:10 +0300

    Updates

---
+++ precache-manifest.f4c05a3d23139653805837f25cf246a8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aaff6958ce3d8549fdebebc32e830ed3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b78196e8b4ecb6e7cc53",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "e9cdc6ee5ae80345261d",
+    "url": "/static/css/main.d3d702b5.chunk.css"
+  },
+  {
+    "revision": "b78196e8b4ecb6e7cc53",
+    "url": "/static/js/2.f087ec37.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.f087ec37.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9cdc6ee5ae80345261d",
+    "url": "/static/js/main.d50e3f55.chunk.js"
+  },
+  {
+    "revision": "d4b36a7767b0ecb063cd",
+    "url": "/static/js/runtime-main.13c409f0.js"
+  },
+  {
+    "revision": "8fa905f97f81069f4ab91a67d2bed508",
+    "url": "/static/media/finance1.8fa905f9.jpg"
+  }
+]);
