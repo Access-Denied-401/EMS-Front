94009a865a6677d24ec906eacf68b32412876fed
ahlamalefishat96 <ahlam.alefishat@gmail.com>
2020-07-27 10:35:02 +0300

    start working on about us

--- src/components/Home/home.js
+++ src/components/Home/home.js
@@ -2,8 +2,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import Slider from '../slider/slider';
-import Header from '../Header/header';
-import Footer from '../Footer/footer';
+import Header from '../header/header';
+import Footer from '../footer/footer';
     
 
 const Home = (props) => {
