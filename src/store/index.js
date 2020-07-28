5c04b75b06f5df497e7c0d278715191513a7f6dc
Abdallah <eng.abdallahobaid@gmail.com>
2020-07-28 16:57:26 +0300

    sadsa

--- src/store/index.js
+++ src/store/index.js
@@ -10,12 +10,13 @@ import Admin from './administration';
 import Financial from './financial';
 import Profile from './profile';
 import Home from './home';
+import loginReducer from './log-in';
 
 // reducer 
 // reducer  ------> in on main reducer ----> dealing with the store 
 // reducer
 
-let reducers = combineReducers({ Admin,Financial,Profile,Home});
+let reducers = combineReducers({ Admin,Financial,Profile,Home,loginReducer});
 
 // create my store and pass this reducers variable
 
