5f5a0425045eda253109612d65a6bc955530c94e
Amer-bit <ny.1995@outlook.com>
2020-07-28 09:16:07 +0300

    sing-in sign-up

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,24 +1,16 @@
 /* eslint-disable no-unused-vars */
+import superagent from 'superagent';
 let signUpUrl = 'https://ems-access-denied.herokuapp.com/signup';
 let signInUrl = 'https://ems-access-denied.herokuapp.com/signin';
 
 // SignUp Action
-export const userSignUp = user => {
-  console.log(user,'llllllllllllllllllllllllllllllllll');
-    // return dispatch => {
-    //   return fetch (signUpUrl, {
-    //     method: 'POST',
-    //     headers: {
-    //       'Content-Type': 'application/json',
-    //       Accept: 'application/json',
-    //     },
-    //     body: JSON.stringify(user),
-    //   })
-    //     .then(res => {
-    //       console.log(res);
-    //     })
-    //     .catch(error => console.log(error));
-    // };
+export const userSignUp = user => async dispatch => {
+  return superagent.post(signUpUrl)
+    .send(user)
+    .then(res => {
+      console.log(res);
+    })
+    .catch(error => console.log(error));
 };
 
 
