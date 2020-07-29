d3d1fd6102c8acd4a6e8732caf9ed53b844ec269
Amer-bit <ny.1995@outlook.com>
2020-07-29 14:37:38 +0300

    trying to Deploy

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
-import Header from './components/Header/header';
+// import Header from './components/Header/header';
 import Footer from './components/Footer/footer';
 import Routes from './router';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -9,7 +9,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
     <>
-      <Header />
+      {/* <Header /> */}
       <Routes />
       <Footer />     
     </>
