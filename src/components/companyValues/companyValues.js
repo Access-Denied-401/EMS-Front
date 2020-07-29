89593e01c804b363e221d49c68b3e2edd52dea3b
raghadalquran <raghadalquran1@gmail.com>
2020-07-29 18:18:42 +0300

    pull

--- src/components/companyValues/companyValues.js
+++ src/components/companyValues/companyValues.js
@@ -31,7 +31,7 @@ const CompanyValues = (props) => {
               <div className=" py-0 my-0 bg-white border-0">
                 <div className="row justify-content-center">
                   <div className="col-auto">
-                    <img className="img-fluid-logo " src="" width="60" height="60" /></div>
+                    <img className="img-fluid-logo " src="https://files.slack.com/files-pri/TNGRRLUMA-F017G7P1U3H/ems.png" width="60" height="60" /></div>
                 </div>
               </div>
             </div>
