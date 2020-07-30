a572f6220132550f8e91a49a5f530cf9d30ec0b9
Amer-bit <ny.1995@outlook.com>
2020-07-30 03:48:20 +0300

    header & footer route

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
-import Header from './components/Header/header';
-import Footer from './components/Footer/footer';
+// import Header from './components/Header/header';
+// import Footer from './components/Footer/footer';
 import Routes from './router';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -9,9 +9,9 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
     <>
-      <Header />
+      {/* <Header /> */}
       <Routes />
-      <Footer />     
+      {/* <Footer />      */}
     </>
   );
 }
