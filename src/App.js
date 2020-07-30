843a2a7a2cd30c3852e2699607a1ae41f813b939
Amer-bit <ny.1995@outlook.com>
2020-07-30 11:06:10 +0300

    changed header and footer folder names

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
-import Header from './components/Header/header';
-import Footer from './components/Footer/footer';
+import Header from './components/header/header';
+import Footer from './components/footer/footer';
 import Routes from './router';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
 
 function App() {
   return (
