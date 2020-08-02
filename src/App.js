4c3f6cb3df3326fd73d75a6ec3756563a71af23f
Amer-bit <ny.1995@outlook.com>
2020-08-02 17:30:08 +0300

    refactoredFordeployment

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@
 import React from 'react';
 import Header from './components/header/header';
 import Footer from './components/footer/footer';
-import Routes from './router';
+// import Routes from './router';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <>
       <Header />
-      <Routes />
+      {/* <Routes /> */}
       <Footer />     
     </>
   );
