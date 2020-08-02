f60e2547fc9d20af55b62add2f016ca650cb345b
Amer-bit <ny.1995@outlook.com>
2020-08-02 23:47:25 +0300

    dployBuild

--- src/router.js
+++ src/router.js
@@ -5,12 +5,12 @@ import Home from './components/Home/home';
 import Administration from './components/Administration/administrationhome/administration';
 import AdminAddUser from './components/Administration/admin-add-user/admin-add-user';
 import AdminAcceptUser from './components/Administration/admin-accept-user/admin-accept-user';
-// import AdminEditUser from './components/Administration/admin-edit-user/admin-edit-user';
-// import FinancialManagement from './components/Financial-Management/financial';
-// import Profile from './components/Profile/profile/profile';
-// import EditProfile from './components/Profile/edit-profile/editProfile';
-// import Feedback from './components/Profile/vacation/feedback';
-// import Signin from './components/Sign-in/signin';
+import AdminEditUser from './components/Administration/admin-edit-user/admin-edit-user';
+import FinancialManagement from './components/Financial-Management/financial';
+import Profile from './components/Profile/profile/profile';
+import EditProfile from './components/Profile/edit-profile/editProfile';
+import Feedback from './components/Profile/vacation/feedback';
+import Signin from './components/Sign-in/signin';
 import About from './components/About-Us/aboutus';
 
 
@@ -25,17 +25,17 @@ const Routes = (props) => {
 
       <Route path='/administration/acceptuser' component= {AdminAcceptUser}  exact />
 
-      {/* <Route path='/administration/edituser' component= {AdminEditUser} exact /> */}
+      <Route path='/administration/edituser' component= {AdminEditUser} exact />
 
-      {/* <Route path="/financialmanagement" component={FinancialManagement} exact /> */}
+      <Route path="/financialmanagement" component={FinancialManagement} exact />
 
-      {/* <Route path="/profile" component={Profile} exact /> */}
+      <Route path="/profile" component={Profile} exact />
 
-      {/* <Route path="/profile/EditProfile" component={EditProfile} exact /> */}
+      <Route path="/profile/EditProfile" component={EditProfile} exact />
 
-      {/* <Route path="/profile/Feedback" component={Feedback} exact /> */}
+      <Route path="/profile/Feedback" component={Feedback} exact />
 
-      {/* <Route path="/signin" component={Signin} exact /> */}
+      <Route path="/signin" component={Signin} exact />
 
       <Route path="/about-us" component={About} exact />
     </>
