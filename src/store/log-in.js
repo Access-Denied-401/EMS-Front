6163498662e01fefc307f38c5803284185bd4a75
Amer-bit <ny.1995@outlook.com>
2020-08-02 14:27:56 +0300

    modifiedStore

--- src/store/log-in.js
+++ src/store/log-in.js
@@ -1,5 +1,3 @@
-// import loginUser from './actions';
-
 let initialState = {
   loggedIn: false,
   token: '',
@@ -11,7 +9,6 @@ export default (state= initialState, action) => {
   console.log('im in the reducer', action);
   switch(type){
   case 'SET_LOGIN':
-    // console.log('Im in the reducer at the moment ');
     return {...state, 'loggedIn':payload.loggedIn, 'token':payload.token, 'user':payload.user};
 
   case 'LOGOUT_USER':
