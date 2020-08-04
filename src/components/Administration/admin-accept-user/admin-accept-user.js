c5ad8e8211dfc94d977f965797ab9a6d7d05c243
ahlamalefishat96 <ahlam.alefishat@gmail.com>
2020-08-04 23:34:39 +0300

    About us page

--- src/components/Administration/admin-accept-user/admin-accept-user.js
+++ src/components/Administration/admin-accept-user/admin-accept-user.js
@@ -3,16 +3,31 @@
 import React, { useEffect, useState } from 'react';
 import {Link} from 'react-router-dom';
 import { Button } from 'react-bootstrap';
+import Swal from 'sweetalert2';
 
 import useAjax from '../../hooks/ajaxHook';
 import './admin-accept-user.scss';
-
 
 function AdminAcceptUser (props) {
   
   let [users, setUsers] = useState([]);
   const {getTempUsers, acceptUser, rejectUser} = useAjax();
-  
+
+  function alertAccept() {
+    Swal.fire({
+      icon:'success',
+      title: 'Accepted',
+      text: '',
+    });
+  }
+  function alertReject() {
+    Swal.fire({
+      icon:'success',
+      title: 'Rejected',
+      text: '',
+    });
+  }
+
 
   useEffect(()=> {
     try {
@@ -22,6 +37,7 @@ function AdminAcceptUser (props) {
       console.error(error);
     }
   },[]);
+
 
   return(
     <>
@@ -67,8 +83,8 @@ function AdminAcceptUser (props) {
                     <button onClick={()=> rejectUser(value)}>Remove User</button> </div> */}
 
                   <div className="p-2 icons">
-                    <i className="fa fa-check" aria-hidden="true" onClick={()=> acceptUser(value)}><span>Accept</span></i> 
-                    <i className="fa fa-adjust text-danger" onClick={()=> rejectUser(value)} ><span>Reject</span></i> 
+                    <i className="fa fa-check" aria-hidden="true" onClick={()=> acceptUser(value).then(()=>alertAccept())}><span>Accept</span></i> 
+                    <i className="fa fa-adjust text-danger" onClick={()=> rejectUser(value).then(()=>alertReject())} ><span>Reject</span></i> 
                   </div>
                 </td>
               </tr>, 
