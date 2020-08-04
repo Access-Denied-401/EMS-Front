f2ad55c7e3e2fdf989bae465850e254d99fa3057
raghadalquran <raghadalquran1@gmail.com>
2020-08-04 11:51:26 +0300

    working on admin page

--- src/components/Home/slider/slider.js
+++ src/components/Home/slider/slider.js
@@ -18,8 +18,8 @@ const Slider = (props) => {
             alt="First slide"
           />
           <Carousel.Caption>
-            <h3 className="adv">First slide label</h3>
-            <p className="adv">Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+            {/* <h3 className="adv">First slide label</h3>
+            <p className="adv">Nulla vitae elit libero, a pharetra augue mollis interdum.</p> */}
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
@@ -30,8 +30,8 @@ const Slider = (props) => {
           />
 
           <Carousel.Caption>
-            <h3 className="adv">Second slide label</h3>
-            <p className="adv">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            {/* <h3 className="adv">Second slide label</h3>
+            <p className="adv">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p> */}
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
@@ -42,8 +42,8 @@ const Slider = (props) => {
           />
 
           <Carousel.Caption>
-            <h3 className="adv">Third slide label</h3>
-            <p className="adv">Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+            {/* <h3 className="adv">Third slide label</h3>
+            <p className="adv">Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p> */}
           </Carousel.Caption>
         </Carousel.Item>
       </Carousel>
