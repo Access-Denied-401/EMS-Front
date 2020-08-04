fa5d1f9608ec528538914187760b8ad4b85a3d42
Abdallah <eng.abdallahobaid@gmail.com>
2020-08-04 10:57:34 +0300

    Updates

---
+++ precache-manifest.d1c1c5d9d399715e4949da39c7821c25.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5cb433944050e2ec9004b87b5328ea4c",
+    "url": "/EMS-Front/index.html"
+  },
+  {
+    "revision": "5c0e9613efe020fb65df",
+    "url": "/EMS-Front/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "acf717a2122b3cddca70",
+    "url": "/EMS-Front/static/css/main.f681c1c7.chunk.css"
+  },
+  {
+    "revision": "5c0e9613efe020fb65df",
+    "url": "/EMS-Front/static/js/2.c103a07a.chunk.js"
+  },
+  {
+    "revision": "6552f69441a837d03c6e951b36143302",
+    "url": "/EMS-Front/static/js/2.c103a07a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "acf717a2122b3cddca70",
+    "url": "/EMS-Front/static/js/main.ae7d9fff.chunk.js"
+  },
+  {
+    "revision": "446f851e8568e48d40a8",
+    "url": "/EMS-Front/static/js/runtime-main.d5d5bbe1.js"
+  },
+  {
+    "revision": "8f8e2392d8b0cfc42985654d777cbe9d",
+    "url": "/EMS-Front/static/media/new.8f8e2392.png"
+  }
+]);
