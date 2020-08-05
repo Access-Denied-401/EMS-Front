059751991465199ca6a03c1ad5c8224685dbf85c
Amer-bit <ny.1995@outlook.com>
2020-08-05 14:24:43 +0300

    chat

--- src/components/Administration/admin-accept-user/admin-accept-user.js
+++ src/components/Administration/admin-accept-user/admin-accept-user.js
@@ -51,7 +51,7 @@ function AdminAcceptUser (props) {
               </tr>
             </thead>
             <tbody>
-              {users.map (value =><tr key={value._id} className="border-bottom"> 
+              {currentPost.map (value =><tr key={value._id} className="border-bottom"> 
                 <td>
                   <div className="p-2"> <span className="spanEdit font-weight-bold"><img src={value.image} width="40" className="rounded-circle" alt='' /></span> </div>
                 </td>
